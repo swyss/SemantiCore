f9f16b5fdbffd906672510c42000993a83b61363
swyss <stefan.arnold.wyss@gmail.com>
2024-12-03 16:14:41 +0100

    add theme toggle

--- src/SemantiCore/frontend/semanti-core/src/layouts/default.vue
+++ src/SemantiCore/frontend/semanti-core/src/layouts/default.vue
@@ -1,13 +1,32 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-spacer></v-spacer>
 
-    <AppFooter />
-  </v-app>
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="toggleTheme"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <router-view />
+      </v-main>
+      <AppFooter />
+    </v-app>
 </template>
 
 <script setup>
   //
+import AppFooter from "@/components/AppFooter.vue";
+//
+  import { useTheme } from 'vuetify'
+
+  const theme = useTheme()
+
+  function toggleTheme () {
+    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  }
 </script>
--- src/SemantiCore/frontend/semanti-core/src/pages/index.vue
+++ src/SemantiCore/frontend/semanti-core/src/pages/index.vue
@@ -4,4 +4,5 @@
 
 <script setup>
   //
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
